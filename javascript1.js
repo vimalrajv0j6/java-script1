eab23d5ca10d5f72f3c59abf365667a2f719faeb
vimalrajv0j6 <vimalrajv0r6@gmail.com>
2025-06-03 10:40:26 +0530

    js function

--- javascript1.js
+++ javascript1.js
@@ -4,3 +4,14 @@ function add(){
     console.log(a+b);
 }
 add();
+
+
+var factor = "dhanush";
+var fplayer = "dhoni";
+var fmovie = "puthuppettai"
+
+function favourite() {
+    console.log("Favourite Actor :" (factor));
+    console.log("Favourite Player :"(fplayer));
+    console.log("Favourite Movie :"(fmovie));
+}
